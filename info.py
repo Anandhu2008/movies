af81a58a391318347ddf8a72137c47da0e3c0bfe
Anandhu2008 <anandhukj400@gmail.com>
2025-02-10 19:44:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7791060079:AAGuAG39lXUQJl91EMlKhhFUwVbRP4X
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5665677555').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/anandhuyoooo") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002381851296'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', '1002271573475')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002271573475').split()]
